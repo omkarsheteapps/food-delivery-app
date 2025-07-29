92e5c316635ff0c97276ad1129ab88fda788685e
omkarsheteapps <believekiddo@gmail.com>
2025-07-29 21:23:25 +0530

    images src fix

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,5 +1,16 @@
 import React from "react";
 import MenuCard from "../layouts/MenuCard";
+import img1 from "../assets/img/menu1.png";
+import img2 from "../assets/img/menu2.png";
+import img3 from "../assets/img/menu3.png";
+import img4 from "../assets/img/menu4.png";
+import img5 from "../assets/img/menu5.png";
+import img6 from "../assets/img/menu6.png";
+import img7 from "../assets/img/menu7.png";
+import img8 from "../assets/img/menu8.png";
+
+
+
 
 const dishes = [
   {
@@ -7,56 +18,56 @@ const dishes = [
     name: "Chicken Burger",
     price: "₹220",
     tag: "Hot",
-    image: "src/assets/img/menu1.png",
+    image: img1,
   },
   {
     id: 2,
     name: "Peri Peri Pizza",
     price: "₹400",
     tag: "New",
-    image: "src/assets/img/menu2.png",
+    image: img2,
   },
   {
     id: 3,
     name: "Sushi Deluxe",
     price: "₹330",
     tag: "",
-    image: "src/assets/img/menu3.png",
+    image: img3,
   },
   {
     id: 4,
     name: "Paneer Poppers",
     price: "₹220",
     tag: "Hot",
-    image: "src/assets/img/menu4.png",
+    image: img4,
   },
   {
     id: 5,
     name: "Club Sandwich",
     price: "₹100",
     tag: "",
-    image: "src/assets/img/menu5.png",
+    image: img5,
   },
   {
     id: 6,
     name: "Mocktail Mojito",
     price: "₹150",
     tag: "New",
-    image: "src/assets/img/menu6.png",
+    image: img6,
   },
   {
     id: 7,
     name: "Nacho Fiesta",
     price: "₹80",
     tag: "Hot",
-    image: "src/assets/img/menu7.png",
+    image: img7,
   },
   {
     id: 8,
     name: "Biryani Bowl",
     price: "₹300",
     tag: "",
-    image: "src/assets/img/menu8.png",
+    image: img8,
   },
 ];
 
