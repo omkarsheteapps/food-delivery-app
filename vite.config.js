a7ba9fc3eeb48688defeda8afacc6f7b5fee709a
omkarsheteapps <believekiddo@gmail.com>
2025-07-29 20:04:18 +0530

    config updated

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [tailwindcss(),react()],
-    base:'/home/',
+    base:'/food-delivery-app/',
     server: {
         open: true,
         port:3001,
